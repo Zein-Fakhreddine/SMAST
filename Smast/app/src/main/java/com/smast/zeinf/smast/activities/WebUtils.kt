4244dcb3a59c84edff0bd76c1e4a4e22c9f61437
ZeinF <1Astronomy>
2017-07-08 22:12:02 -0500

    added a way to programmatically add more subreddits to use

--- Smast/app/src/main/java/com/smast/zeinf/smast/activities/WebUtils.kt
+++ Smast/app/src/main/java/com/smast/zeinf/smast/activities/WebUtils.kt
@@ -1,6 +1,8 @@
 package com.smast.zeinf.smast.activities
 
 
+import android.text.Html
+import com.smast.zeinf.smast.BuildConfig
 import org.json.JSONException
 import org.json.JSONObject
 import java.net.URL
@@ -8,18 +10,26 @@ import java.util.*
 
 object WebUtils {
 
-    private val SMMRY_API_KEY = "C767F04C97"
+    private val SMMRY_API_KEY = BuildConfig.SMMRY_API_KEY
     private val SENTENCE_LENGTH = 5
-    private val ARTICLE_LENGTH = 50
+    private val ARTICLE_LENGTH = 25
+    private val subReddits = arrayOf("worldnews", "news")
+
+    @Suppress("DEPRECATION")
+    fun String.fromHtml() : String{
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N)
+            return Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+        else
+            return Html.fromHtml(this).toString()
+    }
 
     fun getRedditPosts(): ArrayList<Summary.RedditPost> {
         val posts = ArrayList<Summary.RedditPost>()
-
-        val children = JSONObject(URL("https://www.reddit.com/r/worldnews+news/hot.json").readText()).getJSONObject("data").getJSONArray("children")
+        val children = JSONObject(URL("https://www.reddit.com/r/${subReddits.joinToString(separator = "+")}/hot.json").readText()).getJSONObject("data").getJSONArray("children")
         for (i in 0.rangeTo((children.length() - 1))) {
             val data = children.getJSONObject(i).getJSONObject("data")
             try {
-                posts.add(Summary.RedditPost(data.getString("title"), data.getString("url"), data.getString("subreddit"), data.getString("domain"), data.getJSONObject("preview").getJSONArray("images").getJSONObject(0).getJSONObject("source").getString("url")))
+                posts.add(Summary.RedditPost(data.getString("title").fromHtml(), data.getString("url"), data.getString("subreddit"), data.getString("domain"), data.getJSONObject("preview").getJSONArray("images").getJSONObject(0).getJSONObject("source").getString("url")))
             } catch (e: JSONException) {
                 e.printStackTrace()
                 continue
